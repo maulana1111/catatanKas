c7ca023afa841186993df35c5e92acd362f8db27
mdevinfr <devinfrr@gmail.com>
2022-12-13 18:28:27 +0700

    sedang membuat sistem share pdf

---
+++ src/pages/home/home/component/component_html/GeneratePDF.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Alert} from 'react-native';
+
+import RNHTMLtoPDF from 'react-native-html-to-pdf';
+const date = new Date();
+
+function GeneratePDF() {
+  //   const generate = async () => {
+  return Alert.alert('hit');
+  //   };
+  //   generate();
+}
+
+export default GeneratePDF;
